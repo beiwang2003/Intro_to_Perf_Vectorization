55f088b5ba9ccafe890ab3ba2895b46bb7dac5a9
Bei Wang <beiwang@princeton.edu>
2019-04-15 14:08:06 -0400

    fixed inversion bug

--- nbody.cc
+++ nbody.cc
@@ -90,9 +90,9 @@ void MoveParticles(const int nParticles, Particle* const particle, const float d
 #endif
       const float drPower32Inv = 1.0 / drPower32;
       // Calculate the net force
-      Fx += dx * drPower32;  
-      Fy += dy * drPower32;  
-      Fz += dz * drPower32;
+      Fx += dx * drPower32Inv;  
+      Fy += dy * drPower32Inv;  
+      Fz += dz * drPower32Inv;
 
     }
 
